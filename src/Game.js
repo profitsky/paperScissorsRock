672b3c5ab9ccc3184dbd8f4852fc57b5090fc69c
piotr <peter.zyskowski@gmail.com>
2021-05-24 22:31:13 +0200

    SASS/BEM no mobile

--- src/Game.js
+++ src/Game.js
@@ -4,11 +4,16 @@ import {Result} from "../src/app/Result";
 class Game{
 
     currentStep = 0;
-    lastStep = 10;    
+    lastStep = 10;
+    roundsNumber = 10;   
     
 
-    constructor({scoreBoardSelector, choiceWrapper, messageWrapper})
+    constructor({roundsDisplay, setupButton, overlay, startButton, scoreBoardSelector, choiceWrapper, messageWrapper})
     {
+        this.roundsDisplay = roundsDisplay;
+        this.setupButton = setupButton;
+        this.overlay = overlay;
+        this.startButton = startButton;
         this.scoreBoardSelector = scoreBoardSelector;        
         this.choiceWrapper = choiceWrapper;
         this.messageWrapper = messageWrapper;
@@ -36,15 +41,50 @@ class Game{
         this.messageWrapper.innerHTML = "You chose " + `${userChoice}`              
     };
 
-    start()
-    {           
+    pressSetupButton(button){
         
+            if(button.classList.contains("btn--up") && this.roundsNumber < 25)
+            {
+                this.roundsNumber++;
+                this.roundsDisplay.innerHTML = this.roundsNumber;
+            }
+            else if(button.classList.contains("btn--down") && this.roundsNumber > 1)
+            {
+                this.roundsNumber--;
+                this.roundsDisplay.innerHTML = this.roundsNumber;
+            }
+            else
+            {
+                console.log("dzwiek i czerwone litery")
+            }
+    };
+
+    start()
+    {       
+        this.roundsDisplay.innerHTML = this.roundsNumber;  
+        this.startButton.addEventListener("click", ()=>{
+            this.overlay.classList.add("overlay--fade-in")
+        });
+
+        this.setupButton.forEach(button => {
+            button.addEventListener("click", (e) => {
+                this.pressSetupButton(button)
+                button.classList.add("btn--press")
+            });
+        });
+
+        
+
         for(let i =0; i<this.choiceWrapper.length; i++){
         this.choiceWrapper[i].addEventListener("click", (e) => this.getUserChoice(e))}                 
     };
 };
 
-const game = new Game({    
+const game = new Game({
+    roundsDisplay: document.querySelector(".heading-secondary--display"),
+    setupButton: document.querySelectorAll(".btn--setup"),
+    overlay: document.querySelector(".overlay"),
+    startButton: document.querySelector(".btn--start"),
     scoreBoardSelector: document.querySelector(".score-board"),   
     choiceWrapper: [...document.querySelectorAll(".choice")],
     messageWrapper: document.getElementById("player-message")
